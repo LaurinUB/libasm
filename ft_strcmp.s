265d5a3bb96a7bd17e7f86f2d2fab118af1c1c61
luntiet <laurinub@yahoo.de>
2023-12-11 20:17:51 +0100

    update comments

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -3,24 +3,24 @@ section .text
 global _ft_strcmp
 
 _ft_strcmp:
-  mov rax, 0 ; declare count variable to 0
+  mov rax, 0
 
   .loop:
-    mov cl, BYTE [rdi+rax] ; declare char cl with rdi[rax]
-    mov dl, BYTE [rsi+rax] ; declare char dl with rsi[rax]
-    inc rax ; rax++
-    test cl, cl ; test value of cl
-    jnz .cmp ; if cl != '\0' jmp to compare step
-    test dl, dl ; if dl also == '\0' jmp to end
+    mov cl, BYTE [rdi+rax]  ; declare char cl with rdi[rax]
+    mov dl, BYTE [rsi+rax]  ; declare char dl with rsi[rax]
+    inc rax                 ; rax++
+    test cl, cl             ; test value of cl
+    jnz .cmp                ; if cl != '\0' jmp to compare step
+    test dl, dl             ; if dl also == '\0' jmp to end
     jz .end
   .cmp:
-    cmp cl, dl ; cmp values of char cl, and dl
-    je .loop ; if cl == dl return to loop
-    movzx rax, cl ; else put char value of cl to rax
-    movzx rdx, dl ; and char value of dl to rdx
-    sub rax, rdx ; rax - rdx
-    ret ; return difference of character
+    cmp cl, dl              ; cmp values of char cl, and dl
+    je .loop                ; if cl == dl return to loop
+    movzx rax, cl           ; else put char value of cl to rax
+    movzx rdx, dl           ; and char value of dl to rdx
+    sub rax, rdx            ; rax - rdx
+    ret                     ; return difference of character
   
   .end:
-    mov rax, 0 ; set value to 0 => string is equal
+    mov rax, 0              ; set value to 0 => string is equal
     ret
--- ft_strlen.s
+++ ft_strlen.s
@@ -3,16 +3,16 @@ section .text
 global _ft_strlen
 
 _ft_strlen:
-  mov rdx, rdi ; put arg from rdi into rbx
-  mov rax, 0; declare count return variable = 0
+  mov rdx, rdi              ; put arg from rdi into rbx
+  mov rax, 0                ; return variable = 0
 
-  cmp BYTE [rdx], 0 ; if (rdx[0] == /0)
-  je .end ; jump to return;
+  cmp BYTE [rdx], 0         ; if (rdx[0] == /0)
+  je .end                   ; jump to return;
 
-  .loop: ; while (rdx[rax] != '/0')
-    inc rax ; increase value in rax
+  .loop:                    ; while (rdx[rax] != '/0')
+    inc rax                 ; increase value in rax
     cmp BYTE [rdx + rax], 0 ; check for 0 byte (cl == '/0')
-    jne .loop ; continue by jumping back to loop start if cmp not equal
+    jne .loop               ; continue if cmp not equal
 
   .end:
-    ret ; return value stored in rax
+    ret                     ; return value stored in rax
