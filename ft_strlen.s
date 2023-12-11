7b79fd41a229a7742b2e8cb0a26888ab60b039fd
luntiet <laurinub@yahoo.de>
2023-12-11 13:19:04 +0100

    update to different loop

--- ft_strlen.s
+++ ft_strlen.s
@@ -3,14 +3,16 @@ section .text
 global _ft_strlen
 
 _ft_strlen:
-  mov rax, rdi
+  mov rdx, rdi ; put arg from rdi into rbx
+  xor rax, rax ; declare count return variable
 
-  .LOOP:
-    cmp BYTE [rax], 0
-    je .BREAK
-    inc rax
-    jmp .LOOP
+  cmp BYTE [rdx], 0 ; if (rdx[0] == /0)
+  je .end ; jump to return with rax == 0;
 
-  .BREAK:
-    sub rax, rdi
-    ret
+  .loop: ; while (rdx[rax] != '/0')
+    inc rax ; increase value in rax
+    cmp BYTE [rdx + rax], 0 ; check for 0 byte (cl == '/0')
+    jne .loop ; continue by jumping back to loop start;
+
+  .end:
+    ret ; return value stored in rax
