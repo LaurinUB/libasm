0427e434ebc53a3ae03f64b90392f99d5f964d8b
luntiet <laurinub@yahoo.de>
2023-12-11 23:30:51 +0100

    add comments

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -3,20 +3,20 @@ section .text
 global _ft_strcpy
 
 _ft_strcpy:
-  mov rcx, 0
-  mov rax, rdi
-  cmp BYTE [rsi], 0
-  jne .loop
+  mov rcx, 0                  ; count value = 0
+  mov rax, rdi                ; return sring set to dst input
+  cmp BYTE [rsi], 0           ; check if *src == '/0'
+  jne .loop                   ; if not 0 jump too loop
   ret
 
   .loop:
-    mov dl, BYTE [rsi+rcx]
-    cmp dl, 0
-    je .end
-    mov BYTE [rdi+rcx], dl
-    inc rcx
-    jmp .loop;
+    mov dl, BYTE [rsi+rcx]    ; dl = src[rcx]
+    cmp dl, 0                 ; if (dl == 0)
+    je .end                   ; return
+    mov BYTE [rdi+rcx], dl    ; *dst = *src
+    inc rcx                   ; rcx++
+    jmp .loop
 
   .end:
-    mov BYTE [rdi+rcx], 0
+    mov BYTE [rdi+rcx], 0     ; set '/0'
     ret
--- ft_write.s
+++ ft_write.s
@@ -5,18 +5,18 @@ global _ft_write
 extern ___error
 
 _ft_write:
-  mov rax, 0x02000004
-  syscall
-  jc .error
+  mov rax, 0x02000004     ; set value of write in rax
+  syscall                 ; call write
+  jc .error               ; if errorflag jump to .error
   ret
 
   .error:
-    mov rdx, rax
-    push rbp
-    mov rbp, rsp
+    mov rdx, rax          ; safe error code in rdx
+    push rbp              ; init stack
+    mov rbp, rsp          ; point stack to stack top
     call ___error
-    mov DWORD [rax], edx
-    mov rbp, rsp
-    pop rbp
-    mov rax, -1
+    mov DWORD [rax], edx  ; set errno = error
+    mov rsp, rbp          ; set stack top to new stack
+    pop rbp               ; remove stack
+    mov rax, -1           ; return -1
     ret
